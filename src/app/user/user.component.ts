b46d0248761aad878510e26566587092a2b3e33f
parisafro <parisafro.pf@gmail.com>
2020-05-21 19:08:53 +0430

    Initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent{
+  submit(form){
+    var firstName = form.firstName;
+    console.log(firstName);
+  
+    var lastName = form.lastName;
+    console.log(lastName);
+  
+    var comment = form.comment;
+    console.log(comment);
+}
+}
